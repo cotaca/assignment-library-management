b14e2486c9ba6c12ff3e45d322c47644087a5fc8
Viken2004 <kev-syke@outlook.de>
2026-04-06 20:34:37 +0200

    hab mal angefangen

--- sql/07_procedures.sql
+++ sql/07_procedures.sql
@@ -1,1 +1,12 @@
 -- Part 7 - Stored Procedures
+
+CREATE PROCEDURE BorrowBook(member_id INT, book_id INT)
+BEGIN
+    START TRANSACTION
+
+        INSERT INTO Borrowing (borrow_id, borrow_date, return_date, member_id, book_id, borrow_status)
+        -- ab hier DEFINITIV einfach nur hingerotzt
+        VALUES (borrow_counter_seq, CURRENT_DATE, CURRENT_DATE+7, member_id, book_id, ACTIVE)
+
+    COMMIT;
+END;
