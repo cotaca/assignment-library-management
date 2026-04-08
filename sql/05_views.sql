e2e2744d49e0232de5647ef1d8eafb703e3761e7
Cedric Kawczynski <kaw.cedric@web.de>
2026-04-08 11:28:37 +0200

    joa kennt ihr ja

--- sql/05_views.sql
+++ sql/05_views.sql
@@ -2,14 +2,13 @@
 
 -- View 1: CurrentBorrowings
 CREATE VIEW currentBorrowings AS
-SELECT m.last_name, m.first_name, b.title, bw.borrow_date, (CURRENT_DATE - bw.borrow_date) AS days_borrowed
+SELECT m.last_name AS member_last_name, m.first_name AS member_first_name, b.title, bw.borrow_date, (CURRENT_DATE - bw.borrow_date) AS days_borrowed
 FROM Books b
 JOIN Borrowing bw
 ON b.book_id = bw.book_id
 JOIN Members m
-ON bw.member_id = m.member_id;
-
--- Edit Kevin: Sind das nicht ALLE Borrowings und nicht nur die aktuellen?
+ON bw.member_id = m.member_id
+WHERE bw.borrow_status = TRUE;
 
 -- View 2: PopularBooks
 CREATE VIEW popularBooks AS
--- sql/06_functions.sql
+++ sql/06_functions.sql
@@ -5,31 +5,34 @@
 -- • TRUE if available copies exist
 -- • FALSE otherwise
 
--- Ich war mal so frei und habe das Beispiel aus dem Vorlesungssatz genommen
-
 CREATE FUNCTION IsBookAvailable(searched_book_id INT) RETURNS BOOLEAN
-BEGIN
-    DECLARE is_available BOOLEAN;
+    LANGUAGE plpgsql
+    AS $$
+        DECLARE is_available BOOLEAN;
+        BEGIN
 
-    SELECT NOT borrow_status INTO is_available
-    FROM borrowing
-    WHERE book_id = searched_book_id
-    LIMIT 1;
 
-    RETURN is_available;
-END;
+            SELECT NOT borrow_status INTO is_available
+            FROM borrowing
+            WHERE book_id = searched_book_id
+            LIMIT 1;
 
+            RETURN is_available;
+        END;
+    $$;
 
 -- Function 2: BorrowDuration(borrow_id)
 -- Returns number of days between borrow and return (or today).
 
-CREATE FUNCTION BorrowDuration(searched_borrow_id INT)
-BEGIN
-    DECLARE days INT;
+CREATE FUNCTION BorrowDuration(searched_borrow_id INT) RETURNS INT
+    LANGUAGE plpgsql
+    AS $$
+        DECLARE days INT;
+        BEGIN
+            SELECT COALESCE(return_date, CURRENT_DATE) - borrow_date into days
+            FROM borrowing
+            WHERE borrow_id = searched_borrow_id;
 
-    SELECT return_date - borrow_date into days  -- Man müsste noch schauen, dass man unterscheidet zwischen aktuell geliehenen oder abgeschlossenen Leihen
-    FROM borrowing
-    WHERE borrow_id = searched_borrow_id;
-
-    RETURN days;
-END;
+            RETURN days;
+        END;
+    $$;
