f013d375e7493fceac72c3d1228bb594b33694c7
Cedric Kawczynski <kaw.cedric@web.de>
2026-04-08 12:06:28 +0200

    unsere trigger wa

--- sql/08_triggers.sql
+++ sql/08_triggers.sql
@@ -1,1 +1,36 @@
 -- Part 8 - Triggers (Updated)
+
+-- Task 1:
+CREATE TRIGGER preventOverBorrowing
+BEFORE INSERT ON borrowing
+FOR EACH ROW
+EXECUTE FUNCTION checkOverBorrowing();
+
+CREATE FUNCTION checkOverBorrowing()
+RETURNS TRIGGER AS $$
+    BEGIN
+        IF NOT isbookavailable(NEW.book_id) THEN
+            RAISE EXCEPTION 'Buch nicht verfügbar';
+        END IF;
+    END;
+$$ LANGUAGE plpgsql;
+
+-- Task 2:
+ALTER TABLE borrowing
+    ADD COLUMN trans_ref TEXT;
+
+CREATE TRIGGER handleBookReturn
+    AFTER UPDATE ON borrowing
+    FOR EACH ROW
+EXECUTE FUNCTION checkHandleBookReturn();
+
+CREATE FUNCTION checkHandleBookReturn()
+    RETURNS TRIGGER AS $$
+BEGIN
+    IF NOT NEW.return_date >= NEW.borrow_date THEN
+        RAISE EXCEPTION 'Macht keinen Sinn';
+    ELSE
+        UPDATE borrowing SET trans_ref = borrow_date + member_id WHERE borrow_id = NEW.borrow_id; -- borrow_date + member_id muss noch gecastet werden
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
