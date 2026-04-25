0d7c5c95f071b3f3618381ca7daf8e87d7f3e0ad
Viken2004 <kev-syke@outlook.de>
2026-04-25 19:29:42 +0200

    Variationen: Attributreihenfolge

--- sql/01_schema.sql
+++ sql/01_schema.sql
@@ -1,45 +1,45 @@
 -- Part 1 - SQL Table Creation & Constraints
 
 
-DROP TABLE Borrowing CASCADE ;
-DROP TABLE Members CASCADE ;
-DROP TABLE Books CASCADE ;
-DROP TABLE authors CASCADE ;
+DROP TABLE Borrowing;
+DROP TABLE Members;
+DROP TABLE Books;
+DROP TABLE Authors;
 
 -- Task 1.
 CREATE TABLE Authors (
-                         author_id INT PRIMARY KEY,
-                         last_name TEXT NOT NULL,
-                         first_name TEXT NOT NULL,
-                         country TEXT
+    author_id INT PRIMARY KEY,
+    country TEXT,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL
 );
 
 CREATE TABLE Books (
-                       book_id INT PRIMARY KEY,
-                       title TEXT NOT NULL,
-                       genre TEXT,
-                       author_id INT,
-                       published_year INT > 1500,
-                       FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+    book_id INT PRIMARY KEY,
+    genre TEXT,
+    published_year INT CHECK (published_year > 1500),
+    title TEXT NOT NULL,
+    author_id INT,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
 CREATE TABLE Members (
-                         member_id INT PRIMARY KEY,
-                         last_name TEXT NOT NULL,
-                         first_name TEXT NOT NULL,
-                         email TEXT UNIQUE,
-                         join_date DATE DEFAULT CURRENT_DATE
+    member_id INT PRIMARY KEY,
+    email TEXT UNIQUE,
+    join_date DATE DEFAULT CURRENT_DATE,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL
 );
 
 CREATE TABLE Borrowing (
-                           borrow_id INT PRIMARY KEY,
-                           borrow_date DATE NOT NULL DEFAULT CURRENT_DATE,
-                           return_date DATE,
-                           member_id INT,
-                           book_id INT,
-                           FOREIGN KEY (member_id) REFERENCES Members(member_id),
-                           FOREIGN KEY (book_id) REFERENCES Books(book_id),
-                           CHECK (return_date >= borrow_date)
+    borrow_id INT PRIMARY KEY,
+    return_date DATE,
+    borrow_date DATE NOT NULL DEFAULT CURRENT_DATE,
+    book_id INT,
+    member_id INT,
+    FOREIGN KEY (member_id) REFERENCES Members(member_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id),
+    CHECK (return_date >= borrow_date)
 );
 
 -- Task 2.
