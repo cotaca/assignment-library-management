c287fc28ffa6868d770b9ad692b29a0fbef86327
Viken2004 <kev-syke@outlook.de>
2026-04-25 19:56:04 +0200

    Variationen: Umbenennung + Join-Switch

--- sql/06_functions.sql
+++ sql/06_functions.sql
@@ -1,40 +1,42 @@
 -- -- Part 6 - Functions
 --
--- Function 1: IsBookAvailable(book_id)
--- Returns:
--- • TRUE if available copies exist
--- • FALSE otherwise
+-- Function 1: IsBookAvailable
 
-CREATE FUNCTION IsBookAvailable(searched_book_id INT) RETURNS BOOLEAN
+CREATE FUNCTION IsBookAvailable(
+    input_book_id INT
+) RETURNS BOOLEAN
 LANGUAGE plpgsql
 AS $$
-    DECLARE is_available BOOLEAN;
-    BEGIN
+DECLARE
+    result BOOLEAN;
+BEGIN
+    SELECT NOT EXISTS (
+        SELECT 1
+        FROM borrowing
+        WHERE book_id = new_book_id
+        AND borrow_status = TRUE
+    )
+    INTO result;
 
-        SELECT NOT EXISTS (
-            SELECT 1
-            FROM borrowing
-            WHERE book_id = searched_book_id
-              AND borrow_status = TRUE
-        )
-        INTO is_available;
-
-        RETURN is_available;
-    END;
+    RETURN result;
+END;
 $$;
 
--- Function 2: BorrowDuration(borrow_id)
--- Returns number of days between borrow and return (or today).
+-- Function 2: BorrowDuration
 
-CREATE FUNCTION BorrowDuration(searched_borrow_id INT) RETURNS INT
+CREATE FUNCTION BorrowDuration(
+    new_borrow_id INT
+) RETURNS INT
 LANGUAGE plpgsql
 AS $$
-    DECLARE days INT;
-    BEGIN
-        SELECT COALESCE(return_date, CURRENT_DATE) - borrow_date INTO days
-        FROM borrowing
-        WHERE borrow_id = searched_borrow_id;
+DECLARE
+    days_loaned INT;
+BEGIN
+    SELECT COALESCE(return_date, CURRENT_DATE) - borrow_date
+    INTO days_loaned
+    FROM borrowing
+    WHERE borrow_id = input_borrow_id;
 
-        RETURN days;
-    END;
+    RETURN days_loaned;
+END;
 $$;
